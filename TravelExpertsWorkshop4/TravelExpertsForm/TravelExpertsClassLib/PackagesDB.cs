eb0d7e4e4c118023d1a7ab33ca24d78813fd4767
Guido Amaya <guidoamaya@outlook.com>
2019-03-18 16:35:38 -0600

    Update PackagesDB.cs

--- TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/PackagesDB.cs
+++ TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/PackagesDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,75 @@ namespace TravelExpertsClassLib
 {
     class PackagesDB
     {
+        public static class PackagesDB
+        {
+            //method to connect to "Northwind" database and get data from "Orders" table
+            private static SqlConnection GetConnection()
+            {
+                string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True";
+                return new SqlConnection(connectionString);
+            }
+
+            //method for creating an empty list wich will display data
+            public static List<Packages> GetPackages()
+            {
+                List<Packages> packages = new List<Packages>();
+                try
+                {
+                    Packages pkg; // for reading
+
+                    string selectQuery = "SELECT PackageID,  PkgName, PkgStartDate, PkgEndDate, " +
+                                         "        PkgDescription, PkgBasePrice, PkgAgencyCommission " +
+                                         "FROM Packages " +
+                                         "ORDER BY PackageID";
+                    using (SqlConnection con = GetConnection())
+                    {
+                        //use "con" anywhere in this block
+                        using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+                        {
+
+                            //any exception will be thrown to the place where this method was called
+                            con.Open(); // open connection
+
+                            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection); // closes connection when done reading
+
+                            while (dr.Read())// while there is data in data reader
+                            {
+                                pkg = new Packages();
+                                pkg.PackageID = (int)dr["PackageID"];
+                                pkg.PkgName = (string)dr["PkgName"];
+                                pkg.PkgStartDate = (DateTime)dr["PkgStartDate"];
+                                pkg.PkgEndDate = (DateTime)dr["PkgEndDate"];
+
+
+                                // PkgStartDate with exception handling for null values
+                                if (dr["PkgStartDate"] != DBNull.Value)
+                                {
+                                    pkg.PkgStartDate = Convert.ToDateTime(dr["PkgStartDate"]);
+
+                                }
+                                else
+                                {
+                                    pkg.PkgStartDate = null;
+                                }
+
+                                packages.Add(pkg);
+                            }
+                        }
+
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                return packages;
+
+
+
+
+            }
+        }
     }
 }
