156fdefac6420ba3049a4b9cf213c468b0c3af41
EricT135 <eric.truong.135@gmail.com>
2019-03-18 16:39:24 -0600

    Edit PackagesDB

--- TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/PackagesDB.cs
+++ TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/PackagesDB.cs
@@ -1,83 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TravelExpertsClassLib
 {
-    class PackagesDB
+    public class PackagesDB:TravelExpertsDB
     {
-        public static class PackagesDB
+    
+        //method for creating an empty list wich will display data
+        public static List<Packages> GetPackages()
         {
-            //method to connect to "Northwind" database and get data from "Orders" table
-            private static SqlConnection GetConnection()
+            List<Packages> packages = new List<Packages>();
+            try
             {
-                string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True";
-                return new SqlConnection(connectionString);
-            }
+                Packages pkg; // for reading
 
-            //method for creating an empty list wich will display data
-            public static List<Packages> GetPackages()
-            {
-                List<Packages> packages = new List<Packages>();
-                try
+                string selectQuery = "SELECT PackageID,  PkgName, PkgStartDate, PkgEndDate, " +
+                                     "        PkgDescription, PkgBasePrice, PkgAgencyCommission " +
+                                     "FROM Packages " +
+                                     "ORDER BY PackageID";
+                using (SqlConnection con = GetConnection())
                 {
-                    Packages pkg; // for reading
-
-                    string selectQuery = "SELECT PackageID,  PkgName, PkgStartDate, PkgEndDate, " +
-                                         "        PkgDescription, PkgBasePrice, PkgAgencyCommission " +
-                                         "FROM Packages " +
-                                         "ORDER BY PackageID";
-                    using (SqlConnection con = GetConnection())
+                    //use "con" anywhere in this block
+                    using (SqlCommand cmd = new SqlCommand(selectQuery, con))
                     {
-                        //use "con" anywhere in this block
-                        using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+
+                        //any exception will be thrown to the place where this method was called
+                        con.Open(); // open connection
+
+                        SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection); // closes connection when done reading
+
+                        while (dr.Read())// while there is data in data reader
                         {
+                            pkg = new Packages();
+                            pkg.PackageID = (int)dr["PackageID"];
+                            pkg.PkgName = (string)dr["PkgName"];
+                            pkg.PkgStartDate = (DateTime)dr["PkgStartDate"];
+                            pkg.PkgEndDate = (DateTime)dr["PkgEndDate"];
 
-                            //any exception will be thrown to the place where this method was called
-                            con.Open(); // open connection
 
-                            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection); // closes connection when done reading
-
-                            while (dr.Read())// while there is data in data reader
+                            // PkgStartDate with exception handling for null values
+                            if (dr["PkgStartDate"] != DBNull.Value)
                             {
-                                pkg = new Packages();
-                                pkg.PackageID = (int)dr["PackageID"];
-                                pkg.PkgName = (string)dr["PkgName"];
-                                pkg.PkgStartDate = (DateTime)dr["PkgStartDate"];
-                                pkg.PkgEndDate = (DateTime)dr["PkgEndDate"];
+                                pkg.PkgStartDate = Convert.ToDateTime(dr["PkgStartDate"]);
 
-
-                                // PkgStartDate with exception handling for null values
-                                if (dr["PkgStartDate"] != DBNull.Value)
-                                {
-                                    pkg.PkgStartDate = Convert.ToDateTime(dr["PkgStartDate"]);
-
-                                }
-                                else
-                                {
-                                    pkg.PkgStartDate = null;
-                                }
-
-                                packages.Add(pkg);
                             }
+                            else
+                            {
+                                pkg.PkgStartDate = null;
+                            }
+
+                            packages.Add(pkg);
                         }
-
                     }
+
                 }
-
-                catch (Exception e)
-                {
-                    throw e;
-                }
-                return packages;
-
-
-
-
             }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return packages;
+
+
+
+
         }
     }
 }
--- TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/TravelExpertsDB.cs
+++ TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsClassLib/TravelExpertsDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace TravelExpertsClassLib
 {
     public class TravelExpertsDB
     {
+        //method to connect to "Northwind" database and get data from "Orders" table
+        protected static SqlConnection GetConnection()
+        {
+            string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=True";
+            return new SqlConnection(connectionString);
+        }
     }
 }
