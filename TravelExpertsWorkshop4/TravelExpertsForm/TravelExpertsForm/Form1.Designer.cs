f398c64c7cd9ca6b1933efe51f62ce52f264648f
LimingHong <lhong@ualberta.ca>
2019-03-22 14:39:10 -0600

    nothing changed

--- TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsForm/Form1.Designer.cs
+++ TravelExpertsWorkshop4/TravelExpertsForm/TravelExpertsForm/Form1.Designer.cs
@@ -40,11 +40,11 @@
             System.Windows.Forms.Label productIdLabel;
             System.Windows.Forms.Label supNameLabel;
             System.Windows.Forms.Label supplierIdLabel;
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.Label lblnewid;
             System.Windows.Forms.Label label1;
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.packagesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.packageIdComboBox = new System.Windows.Forms.ComboBox();
             this.pkgAgencyCommissionTextBox = new System.Windows.Forms.TextBox();
@@ -64,6 +64,8 @@
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.MainTabControl = new System.Windows.Forms.TabControl();
             this.ProductTab = new System.Windows.Forms.TabPage();
+            this.txtNewProductName = new System.Windows.Forms.TextBox();
+            this.txtNewProductID = new System.Windows.Forms.TextBox();
             this.btnProEdit = new System.Windows.Forms.Button();
             this.btnProAdd = new System.Windows.Forms.Button();
             this.productsDataGridView = new System.Windows.Forms.DataGridView();
@@ -83,8 +85,6 @@
             this.suppliersDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.txtNewProductID = new System.Windows.Forms.TextBox();
-            this.txtNewProductName = new System.Windows.Forms.TextBox();
             packageIdLabel = new System.Windows.Forms.Label();
             pkgAgencyCommissionLabel = new System.Windows.Forms.Label();
             pkgBasePriceLabel = new System.Windows.Forms.Label();
@@ -217,6 +217,24 @@
             supplierIdLabel.TabIndex = 3;
             supplierIdLabel.Text = "Supplier Id:";
             // 
+            // lblnewid
+            // 
+            lblnewid.AutoSize = true;
+            lblnewid.Location = new System.Drawing.Point(48, 209);
+            lblnewid.Name = "lblnewid";
+            lblnewid.Size = new System.Drawing.Size(121, 20);
+            lblnewid.TabIndex = 44;
+            lblnewid.Text = "New Product Id:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(48, 261);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(149, 20);
+            label1.TabIndex = 45;
+            label1.Text = "New Product Name:";
+            // 
             // packagesBindingSource
             // 
             this.packagesBindingSource.DataSource = typeof(TravelExpertsClassLib.Packages);
@@ -254,10 +272,9 @@
             // 
             // pkgDescRichTextBox
             // 
-            this.pkgDescRichTextBox.Location = new System.Drawing.Point(99, 186);
-            this.pkgDescRichTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pkgDescRichTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.pkgDescRichTextBox.Location = new System.Drawing.Point(66, 121);
+            this.pkgDescRichTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pkgDescRichTextBox.Name = "pkgDescRichTextBox";
             this.pkgDescRichTextBox.ReadOnly = true;
             this.pkgDescRichTextBox.Size = new System.Drawing.Size(442, 146);
@@ -301,38 +318,38 @@
             // 
             // ProSupDataGridview
             // 
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.ProSupDataGridview.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ProSupDataGridview.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.ProSupDataGridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ProSupDataGridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ProdNameCB,
             this.SupNameCB});
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.ProSupDataGridview.DefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.ProSupDataGridview.DefaultCellStyle = dataGridViewCellStyle2;
             this.ProSupDataGridview.Enabled = false;
             this.ProSupDataGridview.Location = new System.Drawing.Point(681, 29);
             this.ProSupDataGridview.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ProSupDataGridview.Name = "ProSupDataGridview";
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.ProSupDataGridview.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.ProSupDataGridview.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.ProSupDataGridview.Size = new System.Drawing.Size(728, 749);
             this.ProSupDataGridview.TabIndex = 32;
             this.toolTip1.SetToolTip(this.ProSupDataGridview, "Click Edit to modify");
@@ -411,15 +428,25 @@
             this.ProductTab.Location = new System.Drawing.Point(4, 29);
             this.ProductTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ProductTab.Name = "ProductTab";
-            this.ProductTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.ProductTab.Size = new System.Drawing.Size(1417, 824);
-            this.ProductTab.Location = new System.Drawing.Point(4, 22);
-            this.ProductTab.Name = "ProductTab";
             this.ProductTab.Padding = new System.Windows.Forms.Padding(3);
-            this.ProductTab.Size = new System.Drawing.Size(942, 531);
+            this.ProductTab.Size = new System.Drawing.Size(1417, 824);
             this.ProductTab.TabIndex = 1;
             this.ProductTab.Text = "Products";
             this.ProductTab.UseVisualStyleBackColor = true;
+            // 
+            // txtNewProductName
+            // 
+            this.txtNewProductName.Location = new System.Drawing.Point(224, 258);
+            this.txtNewProductName.Name = "txtNewProductName";
+            this.txtNewProductName.Size = new System.Drawing.Size(100, 26);
+            this.txtNewProductName.TabIndex = 43;
+            // 
+            // txtNewProductID
+            // 
+            this.txtNewProductID.Location = new System.Drawing.Point(224, 206);
+            this.txtNewProductID.Name = "txtNewProductID";
+            this.txtNewProductID.Size = new System.Drawing.Size(100, 26);
+            this.txtNewProductID.TabIndex = 42;
             // 
             // btnProEdit
             // 
@@ -526,15 +553,11 @@
             // 
             // AddPacIDTB
             // 
-            this.AddPacIDTB.Location = new System.Drawing.Point(243, 42);
+            this.AddPacIDTB.Location = new System.Drawing.Point(162, 27);
             this.AddPacIDTB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.AddPacIDTB.Name = "AddPacIDTB";
             this.AddPacIDTB.ReadOnly = true;
-            this.AddPacIDTB.Size = new System.Drawing.Size(298, 26);
-            this.AddPacIDTB.Location = new System.Drawing.Point(162, 27);
-            this.AddPacIDTB.Name = "AddPacIDTB";
-            this.AddPacIDTB.ReadOnly = true;
-            this.AddPacIDTB.Size = new System.Drawing.Size(200, 20);
+            this.AddPacIDTB.Size = new System.Drawing.Size(200, 26);
             this.AddPacIDTB.TabIndex = 41;
             this.AddPacIDTB.Visible = false;
             // 
@@ -542,23 +565,17 @@
             // 
             this.ActionLabelPac.AutoSize = true;
             this.ActionLabelPac.ForeColor = System.Drawing.Color.SeaGreen;
-            this.ActionLabelPac.Location = new System.Drawing.Point(12, 29);
+            this.ActionLabelPac.Location = new System.Drawing.Point(8, 19);
             this.ActionLabelPac.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ActionLabelPac.Name = "ActionLabelPac";
             this.ActionLabelPac.Size = new System.Drawing.Size(64, 20);
-            this.ActionLabelPac.Location = new System.Drawing.Point(8, 19);
-            this.ActionLabelPac.Name = "ActionLabelPac";
-            this.ActionLabelPac.Size = new System.Drawing.Size(44, 13);
             this.ActionLabelPac.TabIndex = 40;
             this.ActionLabelPac.Text = "Viewing";
             // 
             // CancelPacBtn
             // 
-            this.CancelPacBtn.Location = new System.Drawing.Point(446, 723);
-            this.CancelPacBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.CancelPacBtn.Name = "CancelPacBtn";
-            this.CancelPacBtn.Size = new System.Drawing.Size(112, 35);
             this.CancelPacBtn.Location = new System.Drawing.Point(297, 470);
+            this.CancelPacBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CancelPacBtn.Name = "CancelPacBtn";
             this.CancelPacBtn.Size = new System.Drawing.Size(75, 23);
             this.CancelPacBtn.TabIndex = 38;
@@ -580,10 +597,10 @@
             // 
             // PacAddBtn
             // 
-            this.PacAddBtn.Location = new System.Drawing.Point(134, 743);
+            this.PacAddBtn.Location = new System.Drawing.Point(89, 483);
             this.PacAddBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PacAddBtn.Name = "PacAddBtn";
-            this.PacAddBtn.Size = new System.Drawing.Size(112, 35);
+            this.PacAddBtn.Size = new System.Drawing.Size(75, 23);
             this.PacAddBtn.TabIndex = 36;
             this.PacAddBtn.Text = "Add";
             this.PacAddBtn.UseVisualStyleBackColor = true;
@@ -599,7 +616,7 @@
             this.SuppliersTab.Location = new System.Drawing.Point(4, 29);
             this.SuppliersTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.SuppliersTab.Name = "SuppliersTab";
-            this.SuppliersTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SuppliersTab.Padding = new System.Windows.Forms.Padding(3);
             this.SuppliersTab.Size = new System.Drawing.Size(1417, 824);
             this.SuppliersTab.TabIndex = 2;
             this.SuppliersTab.Text = "Suppliers";
@@ -651,56 +668,6 @@
             this.dataGridViewTextBoxColumn4.DataPropertyName = "SupName";
             this.dataGridViewTextBoxColumn4.HeaderText = "SupName";
             this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            // 
-            // txtNewProductID
-            // 
-            this.txtNewProductID.Location = new System.Drawing.Point(224, 206);
-            this.txtNewProductID.Name = "txtNewProductID";
-            this.txtNewProductID.Size = new System.Drawing.Size(100, 26);
-            this.txtNewProductID.TabIndex = 42;
-            // 
-            // txtNewProductName
-            // 
-            this.txtNewProductName.Location = new System.Drawing.Point(224, 258);
-            this.txtNewProductName.Name = "txtNewProductName";
-            this.txtNewProductName.Size = new System.Drawing.Size(100, 26);
-            this.txtNewProductName.TabIndex = 43;
-            // 
-            // lblnewid
-            // 
-            lblnewid.AutoSize = true;
-            lblnewid.Location = new System.Drawing.Point(48, 209);
-            lblnewid.Name = "lblnewid";
-            lblnewid.Size = new System.Drawing.Size(121, 20);
-            lblnewid.TabIndex = 44;
-            lblnewid.Text = "New Product Id:";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(48, 261);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(149, 20);
-            label1.TabIndex = 45;
-            label1.Text = "New Product Name:";
-            // 
-            this.PacAddBtn.Location = new System.Drawing.Point(89, 483);
-            this.PacAddBtn.Name = "PacAddBtn";
-            this.PacAddBtn.Size = new System.Drawing.Size(75, 23);
-            this.PacAddBtn.TabIndex = 36;
-            this.PacAddBtn.Text = "Add";
-            this.PacAddBtn.UseVisualStyleBackColor = true;
-            this.PacAddBtn.Click += new System.EventHandler(this.PacAddBtn_Click);
-            // 
-            // SuppliersTab
-            // 
-            this.SuppliersTab.Location = new System.Drawing.Point(4, 22);
-            this.SuppliersTab.Name = "SuppliersTab";
-            this.SuppliersTab.Padding = new System.Windows.Forms.Padding(3);
-            this.SuppliersTab.Size = new System.Drawing.Size(942, 531);
-            this.SuppliersTab.TabIndex = 2;
-            this.SuppliersTab.Text = "Suppliers";
-            this.SuppliersTab.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
